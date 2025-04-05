ecc5f82a7bd204c221ac9af0e50242e893ca26c0
Dwane Dunn <dwane.dunn@gmail.com>
2025-04-04 20:14:45 -0400

    First draft of skeleton code

--- src/app.js
+++ src/app.js
@@ -1,11 +1,39 @@
-import "bootstrap";
-import "./style.css";
+import 'bootstrap';
+import './style.css';
 
+import './assets/img/rigo-baby.jpg';
+import './assets/img/4geeks.ico';
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log('Hello Rigo from the console!');
 };
+
+// List of pronouns
+const pronouns = ['the', 'our'];
+// List of adjectives
+const adjectives = ['great', 'big'];
+// List of nouns
+const nouns = ['jogger', 'racoon'];
+// List of extensions
+const extensions = ['.com', '.net', '.us', '.io'];
+// Function to generate a random domain name
+function generateDomainName() {
+  const randomPronoun = pronouns[Math.floor(Math.random() * pronouns.length)];
+  const randomAdjective =
+    adjectives[Math.floor(Math.random() * adjectives.length)];
+  const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+  const randomExtension =
+    extensions[Math.floor(Math.random() * extensions.length)];
+
+  return `${randomPronoun}${randomAdjective}${randomNoun}${randomExtension}`;
+}
+// Function to generate a list of domain names
+function generateDomainNames() {
+  const domainNames = [];
+  for (let i = 0; i < 100; i++) {
+    domainNames.push(generateDomainName());
+  }
+  return domainNames;
+}
+// Function to display the domain names in the console
